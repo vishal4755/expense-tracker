4ab320d4f181186e0da49dbecdd0392542e4c094
Vishal4755 <vishaltipe555@gmail.com>
2026-06-08 11:27:50 +0530

    fix: add port and timeout to db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      connectTimeout: 60000
+    }
   }
 );
 
