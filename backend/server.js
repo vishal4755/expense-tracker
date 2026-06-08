33e2908e1a95217cbc1f191eb3c34655063dbe0d
Vishal4755 <vishaltipe555@gmail.com>
2026-06-08 16:31:14 +0530

    fix: add CORS for netlify

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 const sequelize = require('./config/db');
 const Expense = require('./models/Expense');
 const User = require('./models/User');
@@ -11,8 +10,9 @@ const authRoutes = require('./routes/auth');
 const Budget = require('./models/Budget');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(cors({
+  origin: 'https://daily-expense-tracker-update.netlify.app'
+}));
 app.use(express.json());
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/budget', budgetRoutes);
@@ -24,14 +24,12 @@ sequelize.authenticate()
   })
   .then(() => console.log('Tables ready!'))
   .catch((err) => console.log('Error:', err));
-
 app.get('/', (req, res) => {
   res.json({ 
     message: 'Expense Tracker Backend is running!',
     status: 'OK'
   });
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
